7d96d3c86b521270048a9b789708a80af4b2e691
Jaden243 <144974650+Jaden243@users.noreply.github.com>
2023-10-04 17:46:43 -0400

    MissionDemo_Halo
    Inputted halo feature

---
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour {
+    public GameObject launchPoint;
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint"); // a
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false); // b
+    }
+    void OnMouseEnter()
+    {
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true); // b
+    }
+    void OnMouseExit()
+    {
+        //print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false); // b
+    }
+}
