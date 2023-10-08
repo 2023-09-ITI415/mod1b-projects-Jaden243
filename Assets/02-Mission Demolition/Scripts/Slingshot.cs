3aebb8d0e3b2bf0a25d3820964e95f89ab88409f
Jaden243 <144974650+Jaden243@users.noreply.github.com>
2023-10-08 11:46:01 -0400

    MissionDemo_SlingshotCoding
    Configuring Slingshot to launch

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
+    // fields set in the Unity Inspector pane
+    [Header("Set in Inspector")] // a
+    public GameObject prefabProjectile;
+
+    // fields set dynamically
+    [Header("Set Dynamically")] // a
+
     public GameObject launchPoint;
+    public Vector3 launchPos; // b
+    public GameObject projectile; // b
+    public bool aimingMode; // b
+
     void Awake()
     {
         Transform launchPointTrans = transform.Find("LaunchPoint"); // a
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false); // b
+        launchPos = launchPointTrans.position; // c
     }
     void OnMouseEnter()
     {
@@ -19,5 +31,15 @@ public class Slingshot : MonoBehaviour {
     {
         //print("Slingshot:OnMouseExit()");
         launchPoint.SetActive(false); // b
+    }
+    void OnMouseDown() { // d
+      // The player has pressed the mouse button while over Slingshot
+        aimingMode = true;
+        // Instantiate a Projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        // Start it at the launchPoint
+        projectile.transform.position = launchPos;
+        // Set it to isKinematic for now
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
 }
